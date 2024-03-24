3450bf3fb886f0746cdafe8a4539332485d8f256
Shinoren <raihanmiftah375@gmail.com>
2024-03-24 15:24:29 +0800

    changes to Data Load.sql

--- SQL/2. Data Load.sql
+++ SQL/2. Data Load.sql
@@ -11,6 +11,18 @@ LOAD DATA INFILE "D:/adhim/SEMESTER 4/Manajemen Basis Data/Spotitip V2/Spotitip-
 LOAD DATA INFILE "D:/adhim/SEMESTER 4/Manajemen Basis Data/Spotitip V2/Spotitip-V2/playlist_song_contains.csv" INTO TABLE user FIELDS TERMINATED BY ',' IGNORE 1 LINES;
 
 -- load data raihan
-
+LOAD DATA INFILE "E:/Admin's Files/College Tasks/Semester 4/MBD/Spotitip V2/Spotitip-V2/csv/album_artist_has.csv" INTO TABLE album_artist_has FIELDS TERMINATED BY ',' IGNORE 1 LINES;
+LOAD DATA INFILE "E:/Admin's Files/College Tasks/Semester 4/MBD/Spotitip V2/Spotitip-V2/csv/album.csv" INTO TABLE album FIELDS TERMINATED BY ',' IGNORE 1 LINES;
+LOAD DATA INFILE "E:/Admin's Files/College Tasks/Semester 4/MBD/Spotitip V2/Spotitip-V2/csv/artist.csv" INTO TABLE artist FIELDS TERMINATED BY ',' IGNORE 1 LINES;
+LOAD DATA INFILE "E:/Admin's Files/College Tasks/Semester 4/MBD/Spotitip V2/Spotitip-V2/csv/playlist_song_contains.csv" INTO TABLE playlist_song_contains FIELDS TERMINATED BY ',' IGNORE 1 LINES;
+LOAD DATA INFILE "E:/Admin's Files/College Tasks/Semester 4/MBD/Spotitip V2/Spotitip-V2/csv/playlist.csv" INTO TABLE playlist FIELDS TERMINATED BY ',' IGNORE 1 LINES;
+LOAD DATA INFILE "E:/Admin's Files/College Tasks/Semester 4/MBD/Spotitip V2/Spotitip-V2/csv/song_album_contains.csv" INTO TABLE song_album_contains FIELDS TERMINATED BY ',' IGNORE 1 LINES;
+LOAD DATA INFILE "E:/Admin's Files/College Tasks/Semester 4/MBD/Spotitip V2/Spotitip-V2/csv/song_artist_sing.csv" INTO TABLE song_artist_sing FIELDS TERMINATED BY ',' IGNORE 1 LINES;
+LOAD DATA INFILE "E:/Admin's Files/College Tasks/Semester 4/MBD/Spotitip V2/Spotitip-V2/csv/song.csv" INTO TABLE song FIELDS TERMINATED BY ',' IGNORE 1 LINES;
+LOAD DATA INFILE "E:/Admin's Files/College Tasks/Semester 4/MBD/Spotitip V2/Spotitip-V2/csv/user_artist_follow.csv" INTO TABLE user_artist_follow FIELDS TERMINATED BY ',' IGNORE 1 LINES;
+LOAD DATA INFILE "E:/Admin's Files/College Tasks/Semester 4/MBD/Spotitip V2/Spotitip-V2/csv/user_playlist_create.csv" INTO TABLE user_playlist_create FIELDS TERMINATED BY ',' IGNORE 1 LINES;
+LOAD DATA INFILE "E:/Admin's Files/College Tasks/Semester 4/MBD/Spotitip V2/Spotitip-V2/csv/user_playlist_follow.csv" INTO TABLE user_playlist_follow FIELDS TERMINATED BY ',' IGNORE 1 LINES;
+LOAD DATA INFILE "E:/Admin's Files/College Tasks/Semester 4/MBD/Spotitip V2/Spotitip-V2/csv/user_song_liked.csv" INTO TABLE user_song_liked FIELDS TERMINATED BY ',' IGNORE 1 LINES;
+LOAD DATA INFILE "E:/Admin's Files/College Tasks/Semester 4/MBD/Spotitip V2/Spotitip-V2/csv/user.csv" INTO TABLE user FIELDS TERMINATED BY ',' IGNORE 1 LINES;
 
 
