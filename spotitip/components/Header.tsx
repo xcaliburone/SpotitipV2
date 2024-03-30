a516f042567dbb19bd6968befcf6da96d658968d
adhimrahman <adhimrahaman@gmail.com>
2024-03-31 01:25:31 +0800

    STEP 3: Layouting

--- spotitip/components/Header.tsx
+++ spotitip/components/Header.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { BiSearch } from "react-icons/bi";
+import { HiHome } from "react-icons/hi";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { twMerge } from "tailwind-merge";
+import Button from "./Button";
 
 interface HeaderProps {
     children: React.ReactNode;
@@ -35,10 +38,33 @@ const Header: React.FC<HeaderProps> = ({
                     </button>
                 </div>
 
-                <div>
-                    a
+                <div className="flex md:hidden gap-x-2 items-center">
+                    <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+                        <HiHome size={20} className="text-black" />
+                    </button>
+                    
+                    <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+                        <BiSearch size={20} className="text-black" />
+                    </button>
+                </div>
+
+                <div className="flex justify-between items-center gap-x-4">
+                    <>
+                        <div>
+                            <Button onClick={() => {}} className="bg-transparent text-neutral-300 font-medium">
+                                Sign up
+                            </Button>
+                        </div>
+                        
+                        <div>
+                            <Button onClick={() => {}} className="bg-white px-6 py-2">
+                                Log in
+                            </Button>
+                        </div>
+                    </>
                 </div>
             </div>
+            {children}
         </div>
     );
 }
