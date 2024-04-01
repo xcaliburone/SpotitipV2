40ba6de060680af3693728a3b1638892b2723303
adhimrahman <adhimrahaman@gmail.com>
2024-04-01 20:02:31 +0800

    add: tailwindcss setup

---
+++ spotitip/src/SignUp.js
@@ -0,0 +1,22 @@
+const haveAccountLink = document.getElementById('have-account-link');
+const signUpButton = document.querySelector('.signup button');
+
+function showSignInForm(event) {
+    event.preventDefault();
+    window.location.href = './SignIn.html';
+}
+haveAccountLink.addEventListener('click', showSignInForm);
+
+function handleSignUp(event) {
+    event.preventDefault();
+    const username = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    if (username && email && password) {
+        window.location.href = './index.html';
+    } else {
+        alert('Please fill in all fields.');
+    }
+}
+signUpButton.addEventListener('click', handleSignUp);
---
+++ spotitip/tailwind.config.js
@@ -0,0 +1,13 @@
+// /** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                'spotitip' : '#00FF5B',
+            }
+        },
+    },
+    plugins: [],
+}
+
