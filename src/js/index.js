7597409b330570c12d3f51d2ca029a79997b12be
adhimrahman <adhimrahaman@gmail.com>
2024-04-03 23:59:46 +0800

    fix: repush

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+document.querySelector('.search').addEventListener('click', function() {
+    window.location.href = 'BrowseAll.html'; // Ganti dengan URL halaman Browse All Anda
+});
+
+$(document).ready(function(){
+    $.ajax({
+        url: "form.php",
+        type: "GET",
+        success: function(response){
+            $("#result").html(response);
+        }
+    });
+});
