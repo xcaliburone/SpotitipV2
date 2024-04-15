963ad9c6313bd18b15ee1974e836447d1aa6c366
adhimrahman <adhimrahaman@gmail.com>
2024-04-15 16:38:42 +0800

    add: main profile button

---
+++ public/js/profile.js
@@ -0,0 +1,5 @@
+// Menambahkan event listener ke tombol
+document.getElementById("profileButton").addEventListener("click", function() {
+    // Mengarahkan pengguna ke halaman /profile
+    window.location.href = "/profile";
+});
