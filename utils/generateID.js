1bda8a663a688499d77d6128a6ed02e9cdc818b6
adhimrahman <adhimrahaman@gmail.com>
2024-04-16 20:27:25 +0800

    add:  func generate playlist id

---
+++ utils/generateID.js
@@ -0,0 +1,46 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({ host: 'localhost', user: 'root', password: '', database: 'spotitip' });
+
+function generateNewID(table, type) {              
+    return new Promise((resolve, reject) => {
+        const prefix = type === "user" ? "US" : "AR";
+        const sql = `SELECT MAX(id) AS maxId FROM ${table} WHERE id LIKE '${prefix}%'`;
+        connection.query(sql, (err, result) => {
+            if (err) {
+                reject(err);
+            } else {
+                let maxId = result[0].maxId;
+                if (!maxId) {
+                    maxId = prefix + '01';
+                } else {
+                    const prefixLength = prefix.length;
+                    const numericPart = parseInt(maxId.slice(prefixLength), 10) + 1;
+                    maxId = prefix + (numericPart < 10 ? '0' : '') + numericPart;
+                }
+                resolve(maxId);
+            }
+        });
+    });
+}
+
+function generatePlaylistID() {
+    return new Promise((resolve, reject) => {
+        const sql = "SELECT MAX(id) AS maxId FROM playlist";
+        connection.query(sql, (err, result) => {
+            if (err) {
+                reject(err);
+            } else {
+                let maxId = result[0].maxId;
+                if (!maxId) {
+                    maxId = 'PL01'; // Mulai dari PL01 jika tidak ada ID sebelumnya
+                } else {
+                    const numericPart = parseInt(maxId.slice(2), 10) + 1;
+                    maxId = 'PL' + (numericPart < 10 ? '0' : '') + numericPart;
+                }
+                resolve(maxId);
+            }
+        });
+    });
+}
+
+module.exports = { generateNewID, generatePlaylistID }
