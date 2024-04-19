b592f4493a574cbcc01bcebc821ea436d9552066
adhimrahman <adhimrahaman@gmail.com>
2024-04-20 00:43:43 +0800

    delete: addAlbumToUI

--- public/js/createAlbum.js
+++ public/js/createAlbum.js
@@ -1,48 +1,24 @@
 document.getElementById("createAlbumForm").addEventListener("submit", async (event) => {
     event.preventDefault();
-    const title = document.getElementById("albumName").value;
+    const name = document.getElementById("albumName").value;
     const description = document.getElementById("albumDescription").value;
 
     try {
         const response = await fetch("/createAlbum", {
             method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ title, description }) // Passing title and desc
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ name, description })
         });
 
         if (response.ok) {
-            const album = await response.json();
-            addPlaylistToUI(album);
-            closeModal(); // closeModal function untuk tutup modal
+            const message = await response.text(); // Mendapatkan pesan respons dari server
+            alert(message); // Menampilkan pesan respons
+            // Refresh halaman atau lakukan aksi lain yang sesuai
+            closeModal();
         } else {
-            throw new Error("Failed to create album");
+            console.error("Error creating album:", response.statusText);
         }
     } catch (error) {
         console.error("Error creating album:", error);
     }
 });
-
-async function addAlbumToUI(album) {
-    const albumContainer = document.querySelector(".list-items");
-    
-    try {
-        const isDuplicate = await checkDuplicateAlbum(album.title);
-        
-        if (isDuplicate) {
-            alert("Album with this name already exists. Please choose a different name.");
-            return;
-        }
-        
-        const albumItem = document.createElement("div");
-        albumItem.classList.add("item-playlist", "bg-neutral-800", "w-full", "h-[55px]", "list-none", "rounded-md", "overflow-hidden", "flex", "items-center", "hover:opacity-60", "transition", "hover:cursor-pointer");
-        albumItem.innerHTML = `
-            <img src="images/liked.png" alt="liked songs" class="w-[55px]">
-            <p class="text-neutral-300 ml-3 font-medium">${album.title}</p>
-        `;
-        albumContainer.appendChild(albumItem);
-    } catch (error) {
-        console.error("Error adding album to UI:", error);
-    }
-}
