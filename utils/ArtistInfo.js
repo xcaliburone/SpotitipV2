4aab675af4e273b061bab9b83a5bc52b61e500d5
adhimrahman <adhimrahaman@gmail.com>
2024-04-21 00:42:04 +0800

    add: getAllMyArtists

---
+++ utils/ArtistInfo.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({ host: 'localhost', user: 'root', password: '', database: 'spotitip' });
+
+async function getAllMyArtists(artistId) {
+    return new Promise((resolve, reject) => {
+        const sql = 'SELECT name, email, password, followers FROM artist WHERE id = ?';
+        connection.query(sql, [artistId], (err, results) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+}
+
+module.exports = { getAllMyArtists }
