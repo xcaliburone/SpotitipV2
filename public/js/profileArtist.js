48b8391976bfa49bbfb59e13729a62d2894d4e9d
adhimrahman <adhimrahaman@gmail.com>
2024-04-21 00:42:34 +0800

    add: btn click

---
+++ public/js/profileArtist.js
@@ -0,0 +1,22 @@
+const buttons = document.querySelectorAll('button[data-target]');
+
+document.addEventListener('DOMContentLoaded', function() {
+    const defaultMain = document.getElementById('myArtist');
+    defaultMain.style.display = 'flex';
+    const mains = document.querySelectorAll('main:not(#myArtist)');
+    mains.forEach(main => { main.style.display = 'none'; });
+    document.querySelector('button[data-target="myArtist"]').classList.add('active');
+});
+
+buttons.forEach(button => {
+    button.addEventListener('click', function() {
+        const targetId = this.getAttribute('data-target');
+        const mains = document.querySelectorAll('main');
+        mains.forEach(main => {
+            if (main.id === targetId) { main.style.display = 'flex';
+            } else { main.style.display = 'none'; }
+        });
+        buttons.forEach(btn => { btn.classList.remove('active'); });
+        this.classList.add('active');
+    });
+});
