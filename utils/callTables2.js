e5237e6f2f2fcf5741ac1897f5b07c3857ae82b7
adhimrahman <adhimrahaman@gmail.com>
2024-04-20 22:17:20 +0800

    fix: getAllMySongs, getAllMyAlbums, getAllMyPlaylists

--- utils/callTables2.js
+++ utils/callTables2.js
@@ -1,39 +1,61 @@
-// const mysql = require('mysql');
-// const connection = mysql.createConnection({ host: 'localhost', user: 'root', password: '', database: 'spotitip' });
+const mysql = require('mysql');
+const connection = mysql.createConnection({ host: 'localhost', user: 'root', password: '', database: 'spotitip' });
 
-// async function getAllMySongs() {
-//     return new Promise((resolve, reject) => {
-//         const sql = `
-//             SELECT artist.name AS artist_name, song.title AS song_title
-//             FROM song_artist_sing
-//             INNER JOIN artist ON song_artist_sing.artist_id = artist.id
-//             INNER JOIN song ON song_artist_sing.song_id = song.id;
-//         `;
-//         connection.query(sql, (err, results) => {
-//             if (err) {
-//                 reject(err);
-//             } else {
-//                 resolve(results);
-//             }
-//         });
-//     });
-// }
+async function getAllMySongs(artistId) {
+    return new Promise((resolve, reject) => {
+        const sql = `
+            SELECT s.id, s.name, s.genre, s.duration
+            FROM song_artist_sing sa
+            JOIN song s ON sa.song_id = s.id
+            WHERE sa.artist_id = ?
+        `;
+        connection.query(sql, [artistId], (err, results) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+}
 
-// async function getAllMyAlbums() {
-//     return new Promise((resolve, reject) => {
-//         //const sql = ``;
-//         connection.query(sql, (err, results) => {
-//             if (err) {
-//                 reject(err);
-//             } else {
-//                 resolve(results);
-//             }
-//         });
-//     });
-// }
-// // SELECT a.id, a.title, a.num_song, a.duration FROM album_artist_has aa JOIN album a ON aa.album_id = a.id WHERE aa.artist_id = ?;
+async function getAllMyAlbums(artistId) {
+    return new Promise((resolve, reject) => {
+        const sql = `
+            SELECT a.id, a.name, a.num_song, a.duration
+            FROM album_artist_has aa
+            JOIN album a ON aa.album_id = a.id
+            WHERE aa.artist_id = ?
+        `;
+        connection.query(sql, [artistId], (err, results) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+}
 
-// //              SELECT p.id, p.name, p.num_song, p.duration FROM user_playlist_create upc 
-// //             JOIN playlist p ON upc.playlist_id = p.id WHERE upc.user_id = ?
+async function getAllMyPlaylists(artistId) {
+    return new Promise((resolve, reject) => {
+        const sql = `
+            SELECT p.id, p.name, p.num_song, p.duration
+            FROM user_playlist_create upc
+            JOIN playlist p ON upc.playlist_id = p.id
+            WHERE upc.user_id = ?
+        `;
+        connection.query(sql, [artistId], (err, results) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+}
 
-// module.exports = { getAllMySongs, getAllMyAlbums }
+//              SELECT p.id, p.name, p.num_song, p.duration FROM user_playlist_create upc 
+//             JOIN playlist p ON upc.playlist_id = p.id WHERE upc.user_id = ?
+
+module.exports = { getAllMyAlbums, getAllMySongs, getAllMyPlaylists }
