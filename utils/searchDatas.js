2c075571ead6b6ba0eee50655c8be279963677e6
adhimrahman <adhimrahaman@gmail.com>
2024-04-23 00:37:43 +0800

    add: query sql and func formatResults

---
+++ utils/searchDatas.js
@@ -0,0 +1,33 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({ host: 'localhost', user: 'root', password: '', database: 'spotitip' });
+
+async function getAllDatas(keyword) {
+    return new Promise((resolve, reject) => {
+        const sql = `
+            SELECT name, 'user' AS type FROM user WHERE name LIKE ?
+            UNION SELECT name, 'artist' AS type FROM artist WHERE name LIKE ?
+            UNION SELECT name, 'playlist' AS type FROM playlist WHERE name LIKE ?
+            UNION SELECT name, 'album' AS type FROM album WHERE name LIKE ?
+            UNION SELECT name, 'song' AS type FROM song WHERE name LIKE ?
+        `;
+        connection.query(sql, [`%${keyword}%`, `%${keyword}%`, `%${keyword}%`, `%${keyword}%`, `%${keyword}%`], (err, results) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+}
+
+// Format hasil pencarian dengan menambahkan properti "type"
+function formatResults(results) {
+    return results.map(result => {
+        return {
+            name: result.name,
+            type: result.type // Tambahkan properti "type" sesuai dengan jenis entitas
+        };
+    });
+}
+
+module.exports = { getAllDatas, formatResults };
