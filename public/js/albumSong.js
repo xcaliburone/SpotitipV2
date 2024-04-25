07d018801a211a5e1809ae2beee063d03f2ee338
adhimrahman <adhimrahaman@gmail.com>
2024-04-25 13:27:35 +0800

    add

---
+++ public/js/albumSong.js
@@ -0,0 +1,50 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const albumButton = document.getElementById('aside-albums-button');
+//     const artistScreen = document.getElementById('artistScreen');
+//     const searchScreen2 = document.getElementById('searchscreen2');
+//     const albumSong = document.getElementById('albumSong');
+
+//     // artistScreen.style.display = 'block';
+//     // searchScreen2.style.display = 'none';
+//     // albumSong.style.display = 'none';
+
+//     function hideScreens() {
+//         artistScreen.style.display = 'none';
+//         searchScreen2.style.display = 'none';
+//         albumSong.style.display = 'block';
+//     }
+
+//     albumButton.addEventListener('click', () => {
+//         hideScreens();
+//     });
+// });
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const albumButton = document.getElementById('aside-albums-button');
+//     const homeButton = document.getElementById('gotoHome');
+//     const artistScreen = document.getElementById('artistScreen');
+//     const searchScreen2 = document.getElementById('searchscreen2');
+//     const albumSong = document.getElementById('albumSong');
+
+//     function hideScreens() {
+//         artistScreen.style.display = 'none';
+//         searchScreen2.style.display = 'block';
+//         albumSong.style.display = 'block';
+//     }
+
+//     function hideSongAlbum() {
+//         albumSong.style.display = 'none'
+//         searchScreen2.style.display = 'block';
+//         artistScreen.style.display = 'block';
+//     }
+
+//     albumButton.addEventListener('click', () => {
+//         hideScreens();
+//     });
+
+//     homeButton.addEventListener('click', () => {
+//         hideSongAlbum();
+//     })
+
+
+// });
